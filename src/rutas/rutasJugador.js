f46809e3cc01f6b5957d4ebe0fac345272971670
nataliasanta1254 <nsanta77062@umanizales.edu.co>
2024-09-05 17:49:41 -0500

    rutas
    organizamos las rutas y empezamos la conexion con la base de datos

---
+++ src/rutas/rutasJugador.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const enrutador = express.Router();
+const jugadorControlador = require('../controladores/jugadorControlador');
+
+enrutador.post('/registrar', jugadorControlador.registrarJugador);
+enrutador.get('/obtener',jugadorControlador.obtenerJugadores);
+module.exports = enrutador;
---
+++ src/rutas/rutasJugadorNivel.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const enrutador = express.Router();
+const jugadorNivelControlador = require('../controladores/jugadorNivelControlador');
+
+enrutador.post('/registrarPuntaje',jugadorNivelControlador.registrarPuntaje);
+enrutador.get('/obtener/:cedula',jugadorNivelControlador.obtenerPuntajePorJugador);
+module.exports = enrutador;
